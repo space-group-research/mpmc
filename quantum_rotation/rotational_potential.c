8de95f93a8846b354388f20bb9eaaa5050a03234
keythree@gmail.com <keythree@gmail.com@668d259f-773e-d260-55b0-f7322e6b03dc>
2013-06-08 04:35:37 +0000

    Fixed incorrect angle calculation in align_molecule: a) matrix did not perform desired rotation b) align to z-axis instead of x-axis now to correspond to usual (r,theta,phi) definitions of theta and phi c) numerical noise could lead to com atom being used for angle calculation. com is now disallowed from use here.

--- quantum_rotation/rotational_potential.c
+++ quantum_rotation/rotational_potential.c
@@ -9,7 +9,42 @@ University of South Florida
 
 #include <mc.h>
 
-/* align the molecule with the x-axis for initialization */
+void calc_angle(molecule_t *molecule, double * t, double * p) {
+	double theta, phi;
+	double v[3];
+	atom_t * a1 = molecule->atoms->next;
+
+	v[0] = a1->pos[0] - molecule->com[0];
+	v[1] = a1->pos[1] - molecule->com[1];
+	v[2] = a1->pos[2] - molecule->com[2];
+
+	/* get the current polar coordinates */
+	if(v[2] == 0.0)
+		theta = M_PI/2.0;
+	else
+		theta = acos(v[2]/sqrt(v[0]*v[0] + v[1]*v[1] + v[2]*v[2]));
+
+	if (v[0] == 0) {
+		if (v[1] > 0.0 + SMALL_dR ) phi=M_PI/2.0;
+		else if ( v[1] < 0.0 - SMALL_dR ) phi=3.0/2.0 * M_PI;
+		else phi = 0.0;
+	}
+	else if ( v[1] == 0 ) {
+		if ( v[1] > 0.0 ) phi=0;
+		else phi=M_PI;
+	}
+	else {
+		phi = atan(fabs(v[1]/v[0]));
+		if((v[1]/v[0]) < 0.0) phi *= -1.0;
+	}
+
+	*t = theta;
+	*p = phi;
+	return;
+}
+
+
+/* align the molecule with the z-axis for initialization */
 void align_molecule(molecule_t *molecule) {
 
 	atom_t *atom_ptr;
@@ -38,32 +73,45 @@ void align_molecule(molecule_t *molecule) {
 	}
 
 	/* make sure that our atom chosen for finding the polar coords doesn't happen to be at the origin */
-	for(atom_ptr = molecule->atoms; atom_ptr; atom_ptr = atom_ptr->next)
+	// don't start with the first atom... numerical noise can cause him to not be exactly at the origin
+	if ( ! molecule->atoms->next ) {
+		fprintf(stderr,"error: not enough atomic sites on molecule %d to calculate angular orientation\n", molecule->id);
+		exit(-1);
+	}
+	for(atom_ptr = molecule->atoms->next; atom_ptr; atom_ptr = atom_ptr->next)
 		if(!((atom_ptr->pos[0] == 0.0) && (atom_ptr->pos[1] == 0.0) && (atom_ptr->pos[2] == 0.0))) break;
 
 	/* get the current polar coordinates */
 	if(atom_ptr->pos[2] == 0.0)
-		theta = 0.0;
+		theta = M_PI/2.0;
 	else
 		theta = acos(atom_ptr->pos[2]/sqrt(atom_ptr->pos[0]*atom_ptr->pos[0] + atom_ptr->pos[1]*atom_ptr->pos[1] + atom_ptr->pos[2]*atom_ptr->pos[2]));
-	if((atom_ptr->pos[1]/atom_ptr->pos[0]) > MAXDOUBLE)
-		phi = atan(MAXDOUBLE);
+
+	if (atom_ptr->pos[0] == 0) {
+		if (atom_ptr->pos[1] > 0.0 + SMALL_dR ) phi=M_PI/2.0;
+		else if ( atom_ptr->pos[1] < 0.0 - SMALL_dR ) phi=3.0/2.0 * M_PI;
+		else phi=0.0;
+	}
+	else if ( atom_ptr->pos[1] == 0 ) {
+		if ( atom_ptr->pos[1] > 0.0 ) phi=0;
+		else phi=M_PI;
+	}
 	else {
 		phi = atan(fabs(atom_ptr->pos[1]/atom_ptr->pos[0]));
 		if((atom_ptr->pos[1]/atom_ptr->pos[0]) < 0.0) phi *= -1.0;
 	}
 
 	/* form the inverse rotation matrix and multiply */
-	theta *= -1.0; phi *= -1.0;
-	rotation_matrix[0][0] = cos(phi)*cos(theta);
-	rotation_matrix[0][1] = -sin(phi);
-	rotation_matrix[0][2] = cos(phi)*(-sin(theta));
-	rotation_matrix[1][0] = sin(phi)*cos(theta);
+	rotation_matrix[0][0] = cos(theta)*cos(phi);
+	rotation_matrix[0][1] = cos(theta)*sin(phi);
+	rotation_matrix[0][2] = -sin(theta);
+	rotation_matrix[1][0] = -sin(phi);
 	rotation_matrix[1][1] = cos(phi);
-	rotation_matrix[1][2] = sin(phi)*(-sin(theta));
-	rotation_matrix[2][0] = sin(theta);
-	rotation_matrix[2][1] = 0;
+	rotation_matrix[1][2] = 0;
+	rotation_matrix[2][0] = sin(theta)*cos(phi);
+	rotation_matrix[2][1] = sin(theta)*sin(phi);
 	rotation_matrix[2][2] = cos(theta);
+
 	for(atom_ptr = molecule->atoms, i = 0; atom_ptr; atom_ptr = atom_ptr->next, i++) {
 
 		ii = i*3;
@@ -72,7 +120,6 @@ void align_molecule(molecule_t *molecule) {
 		new_coord_array[ii+2] = rotation_matrix[2][0]*atom_ptr->pos[0] + rotation_matrix[2][1]*atom_ptr->pos[1] + rotation_matrix[2][2]*atom_ptr->pos[2];
 
 	}
-
 
 	/* set the new coordinates and then translate back from the origin */
 	for(atom_ptr = molecule->atoms, i = 0; atom_ptr; atom_ptr = atom_ptr->next, i++) {
@@ -191,14 +238,21 @@ double rotational_potential(system_t *system, molecule_t *molecule, double theta
 		/* backup the molecular coordinates */
 		molecule_backup = copy_molecule(system, molecule);
 
-		/* align the molecule with the x-axis initially */
+		/* align the molecule with the z-axis initially */
 		align_molecule(molecule);
 
 		/* perform the rotation */
 		rotate_spherical(molecule, theta, phi);
 
+
 		/* get the energy for this configuration */
 		potential = energy_no_observables(system);
+
+		//double t, p;
+		//calc_angle(molecule, &t, &p);
+		//printf("DEBUG POT %lf %lf %lf | %lf %lf \n", theta, phi, potential, t, p);
+
+		energy(system);
 
 		/* restore the original atomic coordinates */
 		for(atom_backup = molecule_backup->atoms, atom_ptr = molecule->atoms; atom_backup; atom_backup = atom_backup->next, atom_ptr = atom_ptr->next)

62ac8ae7b3b85943cc3c3b8ff2c3870c096678b1
Luciano Laratelli <luciano@laratelli.com>
2020-10-13 18:54:32 -0400

    Change number of parallel make jobs
    Calculate it based on the number of cores available to better suit
    faster machines.
    
    This commit re-formats compile.sh as a side effect.

--- compile.sh
+++ compile.sh
@@ -7,50 +7,58 @@ cd build
 echo $HOSTNAME | grep "rc.usf.edu"
 
 if [ $? == 0 ]; then
-  module purge
-  module load compilers/gcc/5.1.0
-  module load compilers/intel/2015_cluster_xe
-  if [ "$1" = "cuda" ]; then
-    module load apps/cuda/8.0
-  fi
-  export CC=icc
-  export CXX=icpc
+	module purge
+	module load compilers/gcc/5.1.0
+	module load compilers/intel/2015_cluster_xe
+	if [ "$1" = "cuda" ]; then
+		module load apps/cuda/8.0
+	fi
+	export CC=icc
+	export CXX=icpc
 fi
 
 echo $HOSTNAME | grep "bridges.psc.edu"
 
 if [ $? == 0 ]; then
-  module purge
-  module load gcc/5.3.0
-  if [ "$1" = "cuda" ]; then
-    module load cuda/8.0
-  fi
-  export CC=gcc
-  export CXX=g++
+	module purge
+	module load gcc/5.3.0
+	if [ "$1" = "cuda" ]; then
+		module load cuda/8.0
+	fi
+	export CC=gcc
+	export CXX=g++
 fi
 
 echo $HOSTNAME | grep ".sdsc.edu"
 
 if [ $? == 0 ]; then
-  module purge
-  module purge
-  module load cmake/3.9.1
-  module load gnu/4.9.2
-  if [ "$1" = "cuda" ]; then
-    module load cuda/8.0
-  fi
-  export CC=gcc
-  export CXX=g++
+	module purge
+	module purge
+	module load cmake/3.9.1
+	module load gnu/4.9.2
+	if [ "$1" = "cuda" ]; then
+		module load cuda/8.0
+	fi
+	export CC=gcc
+	export CXX=g++
 fi
 
 if [ "$1" = "debug" ]; then
-  cmake -DQM_ROTATION=OFF -DVDW=OFF -DMPI=OFF -DOPENCL=OFF -DCUDA=OFF -DCMAKE_BUILD_TYPE=Debug -Wno-dev ../
+	cmake -DQM_ROTATION=OFF -DVDW=OFF -DMPI=OFF -DOPENCL=OFF -DCUDA=OFF -DCMAKE_BUILD_TYPE=Debug -Wno-dev ../
 elif [ "$1" = "cuda" ]; then
-  cmake -DQM_ROTATION=OFF -DVDW=OFF -DMPI=OFF -DOPENCL=OFF -DCUDA=ON -DCMAKE_BUILD_TYPE=release -Wno-dev ../
+	cmake -DQM_ROTATION=OFF -DVDW=OFF -DMPI=OFF -DOPENCL=OFF -DCUDA=ON -DCMAKE_BUILD_TYPE=release -Wno-dev ../
 elif [ "$1" = "mpi" ]; then
-  cmake -DQM_ROTATION=OFF -DVDW=OFF -DMPI=ON -DOPENCL=OFF -DCUDA=OFF -DCMAKE_BUILD_TYPE=release -Wno-dev ../
+	cmake -DQM_ROTATION=OFF -DVDW=OFF -DMPI=ON -DOPENCL=OFF -DCUDA=OFF -DCMAKE_BUILD_TYPE=release -Wno-dev ../
 else
-  cmake -DQM_ROTATION=OFF -DVDW=OFF -DMPI=OFF -DOPENCL=OFF -DCUDA=OFF -DCMAKE_BUILD_TYPE=Release -Wno-dev ../
+	cmake -DQM_ROTATION=OFF -DVDW=OFF -DMPI=OFF -DOPENCL=OFF -DCUDA=OFF -DCMAKE_BUILD_TYPE=Release -Wno-dev ../
 fi
 
-make -j4
+NUMCORES=4
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	NUMCORES="$(grep -c ^processor /proc/cpuinfo)"
+	NUMCORES=$(($NUMCORES * 2))
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	NUMCORES="$(sysctl -n hw.ncpu)"
+fi
+
+make -j${NUMCORES}
